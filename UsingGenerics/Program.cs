420d2c2bb88d608ef1f36f35901f524a6953a0ce
Ysunil9 <sunil.pcs24@gmail.com>
2023-01-17 21:13:33 +0530

    Welcome to generics problem

---
+++ UsingGenerics/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace UsingGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Generics Problem\n");
+            FindMaximum findMaximum = new FindMaximum(); 
+            findMaximum.MaxValue(30, 80, 60);
+        }
+    }
+}
