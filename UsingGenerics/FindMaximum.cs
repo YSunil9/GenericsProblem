5e72f7e2b6d8c2f0491a7b56393e95f620d8adb7
Ysunil9 <sunil.pcs24@gmail.com>
2023-01-19 22:17:18 +0530

    [SUNIL]:Refactor All The Three In One Generic Method

--- UsingGenerics/FindMaximum.cs
+++ UsingGenerics/FindMaximum.cs
@@ -24,41 +24,6 @@ namespace UsingGenerics
             }
             else
                 Console.WriteLine("All The Numbers Are Equal");
-        
-        }
-        public void MaxValue(float first, float second, float third)
-        {
-            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
-            {
-                Console.WriteLine($"{first} is greater");
-            }
-            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
-            {
-                Console.WriteLine($"{second} is greater");
-            }
-            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
-            {
-                Console.WriteLine($"{third} is greater");
-            }
-            else
-                Console.WriteLine("All The Values are Equal");
-        }
-        public void MaxValue(string first, string second, string third)
-        {
-            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
-            {
-                Console.WriteLine($"{first} is greater");
-            }
-            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
-            {
-                Console.WriteLine($"{second} is greater");
-            }
-            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
-            {
-                Console.WriteLine($"{third} is greater");
-            }
-            else
-                Console.WriteLine("All The Values are Equal");
         }
 
     }
